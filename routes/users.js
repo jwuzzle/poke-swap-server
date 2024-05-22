f2c32910a7028665e1ded63024552c5354c94d73
Jon Wu <jonathan.wu124@gmail.com>
2024-05-21 22:33:44 -0400

    code for endpoints to create users in sql database and for frontend connection

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require('../controllers/users-controller');
+
+router
+    .route('/register')
+    .post(usersController.register);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,46 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const pokeapiRoutes = require('./routes/pokeapi');
+const usersRoutes = require('./routes/users');
+const knex = require("knex")(require("./knexfile"));
+const jwt = require("jsonwebtoken")
 const PORT = process.env.PORT || 9080
 
 
 app.use(cors());
 app.use(express.json());
+const jsonSecretKey = process.env.jsonSecretKey
 
 app.use('/', pokeapiRoutes);
+app.use('/users', usersRoutes);
+
+app.use((req, res, next) => {
+    if (req.url === "/signup" || req.url === "/login") {
+        next();
+    } else {
+        const token = getToken(req);
+
+        if (token) {
+            console.log('Auth Token:', token);
+            if (jwt.verify(token, jsonSecretKey)) {
+                req.decode = jwt.decode(token);
+                next();
+            } else {
+                res.status(403).json({ error: "Not Authorized." });
+            }
+        } else {
+            res.status(403).json({ error: "No token. Unauthorized." });
+        }
+    }
+});
+
+function getToken(req) {
+    if (!req.headers.authorization) {
+        return;
+    } else {
+        return req.headers.authorization.split(" ")[1];
+    }
+}
 
 
 app.listen(PORT, () => {
